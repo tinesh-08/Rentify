65687550de6030d87b3dbbe6491ba8be9d693422
Tinesh Raj T A <tine20229.it@rmkec.ac.in>
2024-05-21 01:22:52 +0530

    second

--- server/server.js
+++ server/server.js
@@ -6,9 +6,18 @@ const userRoutes = require('./routes/userRoutes');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors( {
+  origin : [""], 
+  methods : ["POST", "GET"], 
+  credentials : true }));
+
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+app.get("/",(req,res)=>{
+  res.json("hello");
+})
 
 // Connect to MongoDB
 mongoose.connect('mongodb+srv://tineshr050:1bmq5exyDrY35BqE@cluster0.prqfmpp.mongodb.net/', {
